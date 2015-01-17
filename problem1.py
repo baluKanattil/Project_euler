30648edfa12d78e8dfd0c0cb1fef11e9a6fee2d7
Neo <eggHeadNeo@users.noreply.github.com>
2015-01-17 18:00:38 +0530

    problem 1 added

---
+++ problem1.py
@@ -0,0 +1,14 @@
+#! /usr/bin/python2
+
+sum=0
+
+for i in range(3,1000,3):
+        sum=sum+i
+
+for i in range(5,1000,5):
+        sum=sum+i
+
+for i in range(15,1000,15):
+        sum=sum-i
+
+print sum
